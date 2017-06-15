9d7c3877980c12c94d0158439a71ad8b9eb83db9
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-06-14 18:09:50 -0600

    user can now create a project

--- seeds/interview.js
+++ seeds/interview.js
@@ -8,5 +8,6 @@ exports.seed = function (knex, Promise) {
         { id: 1, interview_type: 'Unstructured', type_id: 1 },
         { id: 2, interview_type: 'Semi-Structured', type_id: 1 },
         { id: 3, interview_type: 'Structured', type_id: 1 },
+        { id: 4, interview_type: 'Participant Observation', type_id: 2 },
        ]));
 };
